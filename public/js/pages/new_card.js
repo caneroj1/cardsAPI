cc5d368591e46f1dbc212563c20eaf2c495d20a0
caneroj1 <caneroj1@tcnj.edu>
2015-09-10 21:29:52 -0400

    added some error displaying on the new card page.

--- public/js/pages/new_card.js
+++ public/js/pages/new_card.js
@@ -2,6 +2,7 @@ var NewCardPageView = Backbone.View.extend({
   el: '#new-card',
   cardView: new CardView(),
   color: 0,
+  blanks: 0,
 
   events: {
     'submit #new-card-form':  'submitCard',
@@ -11,6 +12,7 @@ var NewCardPageView = Backbone.View.extend({
   },
 
   initialize: function() {
+    this.$('#black-info').css('display', 'none');
     this.listenTo(Cards, 'add', this.submitted);
     cardView = new CardView({ model: new Card({existingCard: false}) });
     this.$('#new-card-body').html(cardView.render().el);
@@ -38,30 +40,73 @@ var NewCardPageView = Backbone.View.extend({
   },
 
   updateBody: function(env) {
-    if (this.color === 0)
-      this.$('.cah-face').html(this.$('#cardBody').val());
-    else
-      this.$('.cah-face').html($.map(this.$('#cardBody').val().split(' '),
+    if (this.color === 0) {
+      var body = this.$('#cardBody').val();
+      if (body.indexOf("_") >= 0)
+        this.$('#blanks-counter').html("No underscores allowed.").css("color", "#ef5350");
+      else
+        this.$('#blanks-counter').html("").css("color", "black");
+      this.$('.cah-face').html(body);
+    }
+    else {
+      var tBlanks = 0;
+      var body = $.map(this.$('#cardBody').val().split(' '),
       function(val, i) {
-        if (/_+[.?!,]?/.test(val))
+        if (/_+[.?!,]?/.test(val)) {
+          tBlanks += 1;
+          if (tBlanks > 3) this.$('#blanks-counter').css("color", "#ef5350");
+          else this.$('#blanks-counter').css("color", "black");
+          this.$('#blanks-counter').html("Blanks: " + tBlanks + "/3");
           return "______" + val;
+        }
         else
           return val;
-      }).join(' '));
+      }).join(' ');
+      this.$('.cah-face').html(body);
+      cardView = new CardView({ model: new Card({existingCard: false, CardType: this.color, CardBody: body, CardBlanks: tBlanks}) });
+      this.$('#new-card-body').html(cardView.render().el);
+    }
   },
 
   changeCard: function(env) {
     var color = $(env.currentTarget).data('color');
     this.color = color;
+    var tBlanks = 0;
     var body = $.map(this.$('#cardBody').val().split(' '),
     function(val, i) {
-      if (color === 1 && /_+[.?!,]?/.test(val))
+      if (color === 1 && /_+[.?!,]?/.test(val)) {
+        tBlanks++;
+        if (tBlanks > 3) this.$('#blanks-counter').css("color", "#ef5350");
+        else this.$('#blanks-counter').css("color", "black");
+        this.$('#blanks-counter').html("Blanks: " + tBlanks + "/3");
         return "______" + val;
-      else
+      }
+      else {
         return val;
+      }
     }).join(' ');
 
-    cardView = new CardView({ model: new Card({existingCard: false, CardType: color, CardBody: body}) });
+    if (color === 0) {
+      if (body.indexOf("_") >= 0)
+        this.$('#blanks-counter').html("No underscores allowed.").css("color", "#ef5350");
+      else
+        this.$('#blanks-counter').html("").css("color", "black");
+    }
+    else {
+      if (tBlanks > 3) this.$('#blanks-counter').css("color", "#ef5350");
+      else this.$('#blanks-counter').css("color", "black");
+    }
+
+    if (color == 1) {
+      this.$('#white-info').css('display', 'none');
+      this.$('#black-info').css('display', 'block');
+    }
+    else {
+      this.$('#white-info').css('display', 'block');
+      this.$('#black-info').css('display', 'none');
+    }
+
+    cardView = new CardView({ model: new Card({existingCard: false, CardType: this.color, CardBody: body, CardBlanks: tBlanks}) });
     this.$('#new-card-body').html(cardView.render().el);
   },
 });
